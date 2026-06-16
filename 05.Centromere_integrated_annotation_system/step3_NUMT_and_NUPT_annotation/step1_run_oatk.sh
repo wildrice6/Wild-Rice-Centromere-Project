c577fb4cf4f0486c7fbe87ff0ca0cec44fb30ed9
wildrice6 <15332685413@163.com>
2026-06-16 16:23:09 +0800

    Delete 05.Centromere_integrated_annotation_system directory

--- 05.Centromere_integrated_annotation_system/step3_NUMT_and_NUPT_annotation/step1_run_oatk.sh
+++
@@ -1,2 +0,0 @@
-# Assembly of organelle genomes from HiFi sequencing data using the OATK software
-oatk -k 1001 -c 300 -t 32 -m embryophyta_mito.fam -p embryophyta_pltd.fam  -o organelle my_genome_prefix.hifi.fastq
